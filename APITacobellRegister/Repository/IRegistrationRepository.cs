79b3bc457f483b237006abd587109522d209374c
Shravya Gorantla <Shravya.Gorantla@gisbizdev.gisbiz.chennai.com>
2021-03-31 14:08:25 +0530

    Add project files.

---
+++ APITacobellRegister/Repository/IRegistrationRepository.cs
@@ -0,0 +1,19 @@
+﻿using APITacobellRegister.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APITacobellRegister.Repository
+{
+    interface IRegistrationRepository
+    {
+        IEnumerable<Registration> GetAllRegistrations();
+        Task<Registration> PutRegistration(Registration item);
+        Task<Registration> PostRegistration(Registration item);
+        //Registration GetRegistrationById(int id);
+        
+        Task<Registration> RemoveRegistration(int id);
+
+    }
+}
---
+++ APITacobellRegister/Repository/RegistrationRepository.cs
@@ -0,0 +1,87 @@
+﻿using APITacobellRegister.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APITacobellRegister.Repository
+{
+    public class RegistrationRepository : IRegistrationRepository
+    {
+        private readonly tacobellContext _context;
+        
+        //public Registration(tacobellContext context)
+        //{
+        //    _context = context;
+        //}
+
+        // GET: api/Registrations
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Registration>>> GetRegistrations()
+        {
+            return await _context.Registrations.ToListAsync();
+        }
+
+        // GET: api/Registrations/5
+        
+        private bool RegistrationExists(int id)
+        {
+            return _context.Registrations.Any(e => e.RegistrationId == id);
+        }
+
+        public IEnumerable<Registration> GetAllRegistrations()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Registration> PutRegistration(Registration item)
+        {
+            throw new NotImplementedException();
+        }
+      
+
+
+        public Task<Registration> PostRegistration(Registration item)
+        {
+            throw new NotImplementedException();
+        }
+        public async Task<Registration> PostSpecification(Registration item)
+        {
+            Registration Sp = null;
+            if (item == null)
+            {
+                throw new NullReferenceException();
+            }
+            else
+            {
+                Sp = new Registration() { RegistrationId = item.RegistrationId };
+                await _context.Registrations.AddAsync(Sp);
+                await _context.SaveChangesAsync();
+            }
+            return Sp;
+        }
+
+        Task<Registration> IRegistrationRepository.RemoveRegistration(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public async Task<Registration> RemoveSpecification(string id)
+        {
+            Registration sp = await _context.Registrations.FindAsync(id);
+            if (sp == null)
+            {
+                throw new NullReferenceException();
+            }
+            else
+            {
+                _context.Registrations.Remove(sp);
+                await _context.SaveChangesAsync();
+            }
+            return sp;
+        }
+    }
+}
+
+
